a28e0774d79bc839ee445a48184382c0c3385702
Hung. Nguyen Manh - CMC Global DU3.1 <nmhung@cmcglobal.vn>
2023-02-02 17:02:46 +0700

    flat structure

--- main.go
+++ main.go
@@ -10,8 +10,20 @@ func main() {
 
 	app.Use(logger.New())
 
+	app.Static("/", "./public")
+
+	app.Use(func(c *fiber.Ctx) error {
+
+		return c.Next()
+	})
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
+	})
+
+	// Last middleware to match anything
+	app.Use(func(c *fiber.Ctx) error {
+		return c.SendStatus(404) // => 404 "Not Found"
 	})
 
 	app.Listen(":3000")
